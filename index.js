e3e8b86f362b7155e1fedc9de4fa52c95444cd90
Sajjad Anwar <me@sajjad.in>
2015-05-22 15:35:15 +0530

    async and make phantom requests in series

--- index.js
+++ index.js
@@ -1,60 +1,73 @@
-// filesystem
 var fs = require('fs');
 var http = require('http');
 var phantom = require('phantom');
+var async = require('async');
 
 var places = [];
-// window.current = {};
+var collection = [];
+var isLast = false;
 
 var places = fs.readFileSync('cities.list').toString().split("\n");
-for(i in places) {
+for (var i = 0; i < places.length; i++) {
     place = places[i];
-    if (places[i]) {
-        console.log(place);
-        geocode(place);
+    if (i == (places.length - 1)) {
+        isLast = true;
     }
-}
+    if (place) {
+        geocode(place, isLast);
+    }
+};
 
-function geocode (place) {
+function geocode (place, isLast) {
     // var nominatimUrl = 'http://nominatim.openstreetmap.org/search/'+place+'?format=json&addressdetails=1&limit=1';
+    // mapbox api.tiles.mapbox.com/v4/geocode/mapbox.places/Pune.json?access_token=pk.eyJ1IjoiZ2VvaGFja2VyIiwiYSI6ImFIN0hENW8ifQ.GGpH9gLyEg0PZf3NPQ7Vrg
     http.get({
-        host: 'nominatim.openstreetmap.org',
+        host: 'api.tiles.mapbox.com',
         port: 80,
-        path: '/search/'+place+'?format=json&addressdetails=1&limit=1',
+        path: '/v4/geocode/mapbox.places/'+place+'.json?access_token=pk.eyJ1IjoiZ2VvaGFja2VyIiwiYSI6ImFIN0hENW8ifQ.GGpH9gLyEg0PZf3NPQ7Vrg',
         method: 'GET'
     }, function(response) {
         var data = '';
         response.on('data', function (d) {
-            // console.log(d);
             data += d;
         });
         response.on('end', function() {
+            var coordinate;
             var parsed = JSON.parse(data);
-            var coordinate = [parsed[0].lat, parsed[0].lon]
-            if (coordinate) {
-                compare(coordinate);
+            if (parsed.features.length > 0) {
+                coordinate = parsed.features[0].center;
+                collection.push({'coord': coordinate, 'place': place});
+                if (isLast) {
+                    async.mapLimit(collection, 1, compareGenerator.compare, function (err, result) {
+                        // do nothing
+                    });
+                }
             }
-            console.log(coordinate);
         });
     });
 }
 
-function compare (coordinate) {
-    var pageUrl = 'http://lxbarth.com/compare/#14/'+coordinate[0]+'/'+coordinate[1];
-    phantom.create("--ignore-ssl-errors=yes", "--ssl-protocol=any", function (ph) {
-        ph.createPage(function (page) {
-            page.set('viewportSize', {width:1280,height:900}, function(){
-                page.set('clipRect', {top:0,left:0,width:1280,height:900}, function(){
-                    page.open(pageUrl, function(status) {
-                        setTimeout(function(){
-                            page.render('screenshot.png', function(finished){
-                                console.log('rendering '+pageUrl+' done');
-                                ph.exit();
-                            });
-                        }, 6000);
+
+var compareGenerator = {
+    compare: function compare (data, callback) {
+        console.log('comparing '+data.place);
+        var compareUrl = 'http://lxbarth.com/compare/#14/'+data.coord[1]+'/'+data.coord[0];
+        phantom.create("--ignore-ssl-errors=yes", "--ssl-protocol=any", function (ph) {
+            ph.createPage(function (page) {
+                page.set('viewportSize', {width:1280, height:900}, function(){
+                    page.set('clipRect', {top:0, left:0, width:1280, height:900}, function(){
+                        page.open(compareUrl, function(status) {
+                            setTimeout(function(){
+                                page.render(data.place+'.png', function(finished){
+                                    console.log('rendering '+compareUrl+' done');
+                                    ph.exit();
+                                    callback(null, compareUrl);
+                                });
+                            }, 6000);
+                        });
                     });
                 });
             });
         });
-    });
+    }
 }
